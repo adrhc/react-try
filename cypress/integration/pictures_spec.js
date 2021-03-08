3ef587dfe16c374bffdcd14e1378dccf065ccf46
adrian.petre <adrian.petre@servexternos.gruposantander.com>
2021-03-08 13:00:03 +0000

    pictures

--- cypress/integration/pictures_spec.js
+++ cypress/integration/pictures_spec.js
@@ -1,7 +1,7 @@
 describe("My First Test", () => {
 	it("Visits the Kitchen Sink", () => {
-    cy.visit("https://example.cypress.io");
-    
-    cy.contains('type').click();
+		cy.visit("https://adrhc.go.ro/pictures");
+
+		// cy.contains('type').click();
 	});
 });
